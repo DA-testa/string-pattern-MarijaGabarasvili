43e03c0d217b5a89e198143b5d3bedbe9b9b172b
Mar_tt <63107828+MarijaGabarasvili@users.noreply.github.com>
2023-04-16 10:21:24 +0000

    commit maybe now

--- hash_substring.py
+++ hash_substring.py
@@ -38,7 +38,7 @@ def get_occurrences(pattern, text):
 
     len_of_pattern = len(pattern)
     len_of_text = len(text)
-    result = " "
+    result = ""
     multiplier = 1
     hash_for_pattern = 0
     hash_fot_text = 0
@@ -56,7 +56,7 @@ def get_occurrences(pattern, text):
                     break;
             j=j+1
             if j==len_of_pattern:
-                result = result+str(i)+""
+                result = result+str(i)+" "
 
 
         if(i<len_of_text-len_of_pattern):
